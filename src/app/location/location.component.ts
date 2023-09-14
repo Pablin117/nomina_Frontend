332498b35f93219a5983230d2adb9f16895d6d23
ccaceres <ccaceres@bytesw.com>
2023-09-13 22:03:22 -0600

    complementos de locations

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -10,81 +10,126 @@ import { Router } from "@angular/router";
 })
 export class LocationComponent {
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient,
+    private router: Router
+  ) { }
 
-  ngOnInit(){
-    this.locationData()
-    this.CompanyData()
+  ngOnInit() {
+    this.CompanyService()
+    this.locationService()
   }
 
 
-//variables
-locationsData: any = [];
-BussinessRules: any = [];
-modificar:boolean =true;
-url: String = "http://localhost:4042/v1"
+  //variables
+  locationsData: any = [];
+  companyData: any = [];
+  modify: boolean = false;
+  add: boolean = false;
+  tab: boolean = true
+  locationDataCreate: any = {}
+  dataIndex: any = {}
+  url: String = "http://localhost:4042/v1"
 
-locationData() {
-  this.RequestLocation().subscribe(
-    (response: any) => this.ResponseLocation(response)
-  )
-}
 
-RequestLocation() {
-  var httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
+  locationService() {
+    this.RequestLocation().subscribe(
+      (response: any) => this.ResponseLocation(response)
+    )
   }
-  return this.http.get<any>(this.url + "/location", httpOptions).pipe(
-    catchError(e => "1")
-  )
-}
 
-ResponseLocation(response: any) {
-  this.locationsData = response
-  console.log("Se obtuvo roles")
-}
-
-CompanyData() {
-  this.RequestCompany().subscribe(
-    (response: any) => this.ResponseCompany(response)
-  )
-}
-
-RequestCompany() {
-  var httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
+  RequestLocation() {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.get<any>(this.url + "/location", httpOptions).pipe(
+      catchError(e => "1")
+    )
   }
-  return this.http.get<any>(this.url + "/bussinesRules", httpOptions).pipe(
-    catchError(e => "1")
-  )
-}
 
-ResponseCompany(response: any) {
-  this.BussinessRules = response
-  console.log("Se obtuvo configuracion de empresa")
+  CompanyService() {
+    this.RequestCompany().subscribe(
+      (response: any) => this.ResponseCompany(response)
+    )
+  }
 
-}
+  RequestCompany() {
 
-Confirm(){
-  console.log("confirma")
-  console.log()
-}
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.get<any>(this.url + "/bussinesRules", httpOptions).pipe(
+      catchError(e => "1")
+    )
+  }
 
-
-Modify(){
-  console.log("modifica")
-  console.log()
-}
-
-Add(){
-  console.log("agregar")
-  console.log()
-}
+  ResponseLocation(response: any) {
+    this.locationsData = response;
+    console.log("Se obtuvo sucursales");
+    console.log(this.locationsData)
+  }
 
 
+  ResponseCompany(response: any) {
+    this.companyData = response
+    console.log("Se obtuvo configuracion de empresa")
+    console.log(this.companyData)
+
+  }
+
+  getCompanyName(idCompany: number): string {
+    for (let x = 0; x < this.companyData.length; x++) {
+      if (this.companyData[x].idcompany == idCompany) {
+        return this.companyData[x].name
+      }
+    }
+    return '';
+  }
+
+
+  Modify(index: number) {
+    console.log("modifica")
+    this.dataIndex = this.locationsData[index]
+    this.add = false
+    this.tab = false
+    this.modify = true
+  }
+
+
+  Add() {
+    this.modify = false
+    this.add = true
+    this.tab = false
+    console.log("add")
+    
+  }
+
+  back() {
+    console.log("back")
+    this.modify = false
+    this.add = false
+    this.tab = true
+  }
+
+  addForm() {
+    let formularioValido: any = document.getElementById("addForm");
+    if (formularioValido.reportValidity()) {
+      console.log(this.locationDataCreate)
+
+    }
+  }
+
+  modForm() {
+    let formularioValido: any = document.getElementById("modForm");
+    if (formularioValido.reportValidity()) {
+      console.log(this.locationDataCreate)
+    }
+  }
+
+
+  
 
 }
