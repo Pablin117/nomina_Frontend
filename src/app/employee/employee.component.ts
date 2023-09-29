f8bbe1d115c8da3dbaa15dda0ebba568e4928b7d
ccaceres <ccaceres@bytesw.com>
2023-09-29 14:59:12 -0600

    estados civiles completo

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError } from "rxjs/operators";
+import { Router } from "@angular/router";
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-employee',
@@ -6,5 +10,275 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  ngOnInit() {
+    this.validateSession()
+
+  }
+
+
+  //variables
+  //boolean
+  modify: boolean = false
+  add: boolean = false
+  tab: boolean = true
+  header: boolean = true
+  btnAdd: boolean = false
+  btnUpdate: boolean = false
+  btnDelete: boolean = false
+  print: boolean = false
+  exporte: boolean = false
+
+  //objects
+  maritalStatusDataModify: any = {}
+  maritalStatusDataCreate: any = {}
+  maritalStatusData: any = []
+  dataUser: any = {}
+  options: any = {}
+
+
+
+  // pagina y url
+  url: String = "http://localhost:4042/v1"
+  page: String = "marital-status"
+
+  name = 'employee.xlsx';
+  exportToExcel(): void {
+    let element = document.getElementById('table-consult');
+    const worksheet: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    const book: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(book, worksheet, 'Sheet1');
+
+    XLSX.writeFile(book, this.name);
+  }
+
+  //valida sesiones
+  validateSession() {
+    console.log("valida Sesion")
+    this.dataUser = localStorage.getItem("data")
+    if (this.dataUser != null) {
+      this.dataUser = JSON.parse(this.dataUser)
+      console.log("activo")
+      this.optionsValidate()
+      this.maritalStatusService()
+
+    } else {
+      this.router.navigateByUrl("/")
+    }
+  }
+
+  //bandera de botones
+  optionsValidate() {
+    this.options = localStorage.getItem("options");
+    this.options = JSON.parse(this.options)
+
+    let permisos: any = {}
+    this.options.forEach((item: any) => {
+      if (item.page === this.page) {
+        permisos = item.permisos
+      }
+    })
+    permisos.forEach((item: any) => {
+      this.btnAdd = item.up == 1 ? true : false
+      this.btnUpdate = item.update == 1 ? true : false
+      this.btnDelete = item.down == 1 ? true : false
+      this.print = item.print == 1 ? true : false
+      this.exporte = item.export == 1 ? true : false
+    })
+  }
+
+
+  //Obtiene datos de company
+  maritalStatusService() {
+    this.RequestMaritalStatus().subscribe(
+      (response: any) => this.ResponseMaritalStatus(response)
+    )
+  }
+  RequestMaritalStatus() {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.get<any>(this.url + "/maritalStatus", httpOptions).pipe(
+      catchError(e => "1")
+    )
+  }
+  ResponseMaritalStatus(response: any) {
+    console.log("obtiene status")
+    this.maritalStatusData = response
+  }
+
+  //formulario para modificar
+  modForm() {
+    let formularioValido: any = document.getElementById("modForm")
+    if (formularioValido.reportValidity()) {
+      this.maritalStatusDataModify.userModification = this.dataUser.user
+      this.RequestMaritalStatusUpdate().subscribe(
+        (response: any) => this.ResponseMaritalStatusUpdate(response)
+      )
+    }
+  }
+
+  RequestMaritalStatusUpdate() {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.put<any>(this.url + "/updateMaritalStatus/" + this.maritalStatusDataModify.idMaritalStatus, this.maritalStatusDataModify, httpOptions).pipe(
+      catchError(e => "1")
+    )
+  }
+  ResponseMaritalStatusUpdate(response: any) {
+    if (response.code == 999) {
+      this.revoke()
+    } else if (response.code == 0) {
+      alert(response.message)
+      this.back()
+    } else {
+      alert(response.message)
+    }
+
+  }
+
+
+  //para eliminar
+
+  Delete(response: any) {
+    this.requestDelete(response).subscribe(
+      (response: any) => this.responseDelete(response)
+    )
+  }
+
+  requestDelete(response: any) {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.delete<any>(this.url + "/deleteMaritalStatus/" + response.idMaritalStatus + "/" + this.dataUser.user, httpOptions).pipe(
+      catchError(e => "1")
+    )
+  }
+
+  responseDelete(response: any) {
+
+    if (response.code == 999) {
+
+      this.revoke()
+    } else if (response.code == 0) {
+      alert(response.message)
+      this.back()
+    } else {
+      alert(response.message)
+    }
+  }
+
+  //formulario para agregar 
+
+  addForm() {
+    let formularioValido: any = document.getElementById("addForm");
+    if (formularioValido.reportValidity()) {
+      this.maritalStatusDataCreate.userCreation = this.dataUser.user
+      this.RequestCompanySave().subscribe(
+        (response: any) => this.ResponseCompanySave(response)
+      )
+    }
+  }
+  RequestCompanySave() {
+
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.post<any>(this.url + "/createMaritalStatus", this.maritalStatusDataCreate, httpOptions).pipe(
+      catchError(e => "1")
+    )
+  }
+  ResponseCompanySave(response: any) {
+    if (response.code == 999) {
+
+      this.revoke()
+    } else if (response.code == 0) {
+      alert(response.message)
+      this.back()
+    } else {
+      alert(response.message)
+    }
+
+  }
+
+
+  //banderas
+
+  Add() {
+    this.modify = false
+    this.add = true
+    this.tab = false
+    this.header = false
+  }
+
+
+  Modify(response: any) {
+    this.maritalStatusDataModify = response
+    this.add = false
+    this.tab = false
+    this.modify = true
+    this.header = false
+  }
+
+
+  back() {
+    this.tab = true
+    this.add = false
+    this.modify = false
+    this.header = true
+    this.maritalStatusDataCreate = {}
+    this.maritalStatusDataModify = {}
+    this.ngOnInit()
+  }
+
+  backWelcome() {
+    this.router.navigateByUrl("/home")
+  }
+
+
+  //cierre de sesion
+  revoke() {
+    this.RequestRevoke().subscribe(
+      (response: any) => this.ResponseRevoke(response)
+    )
+  }
+
+
+  RequestRevoke() {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.get<any>(this.url + "/revoke/" + this.dataUser.session, httpOptions).pipe(
+      catchError(e => "1")
+    )
+  }
+
+  ResponseRevoke(response: any) {
+    if (response.code == 0) {
+      alert(response.message)
+      this.router.navigateByUrl("/")
+      localStorage.clear()
+    } else {
+      alert(response.message)
+      this.router.navigateByUrl("/")
+      localStorage.clear()
+    }
+  }
+
+
 
 }
--- src/app/marital-status/marital-status.component.ts
+++ src/app/marital-status/marital-status.component.ts
@@ -34,7 +34,7 @@ export class MaritalStatusComponent {
   //objects
   maritalStatusDataModify: any = {}
   maritalStatusDataCreate: any = {}
-  maritalStatusData: any = {}
+  maritalStatusData: any = []
   dataUser: any = {}
   options: any = {}
 
@@ -43,6 +43,17 @@ export class MaritalStatusComponent {
   // pagina y url
   url: String = "http://localhost:4042/v1"
   page: String = "marital-status"
+
+  name = 'maritalStatus.xlsx';
+  exportToExcel(): void {
+    let element = document.getElementById('table-consult');
+    const worksheet: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    const book: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(book, worksheet, 'Sheet1');
+
+    XLSX.writeFile(book, this.name);
+  }
 
   //valida sesiones
   validateSession() {
@@ -53,7 +64,7 @@ export class MaritalStatusComponent {
       console.log("activo")
       this.optionsValidate()
       this.maritalStatusService()
-   
+
     } else {
       this.router.navigateByUrl("/")
     }
@@ -80,28 +91,194 @@ export class MaritalStatusComponent {
   }
 
 
-    //Obtiene datos de company
-    maritalStatusService() {
-      this.RequestMaritalStatus().subscribe(
-        (response: any) => this.ResponseMaritalStatus(response)
+  //Obtiene datos de company
+  maritalStatusService() {
+    this.RequestMaritalStatus().subscribe(
+      (response: any) => this.ResponseMaritalStatus(response)
+    )
+  }
+  RequestMaritalStatus() {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.get<any>(this.url + "/maritalStatus", httpOptions).pipe(
+      catchError(e => "1")
+    )
+  }
+  ResponseMaritalStatus(response: any) {
+    console.log("obtiene status")
+    this.maritalStatusData = response
+  }
+
+  //formulario para modificar
+  modForm() {
+    let formularioValido: any = document.getElementById("modForm")
+    if (formularioValido.reportValidity()) {
+      this.maritalStatusDataModify.userModification = this.dataUser.user
+      this.RequestMaritalStatusUpdate().subscribe(
+        (response: any) => this.ResponseMaritalStatusUpdate(response)
       )
     }
-    RequestMaritalStatus() {
-      var httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        })
-      }
-      return this.http.get<any>(this.url + "/maritalStatus", httpOptions).pipe(
-        catchError(e => "1")
+  }
+
+  RequestMaritalStatusUpdate() {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.put<any>(this.url + "/updateMaritalStatus/" + this.maritalStatusDataModify.idMaritalStatus, this.maritalStatusDataModify, httpOptions).pipe(
+      catchError(e => "1")
+    )
+  }
+  ResponseMaritalStatusUpdate(response: any) {
+    if (response.code == 999) {
+      this.revoke()
+    } else if (response.code == 0) {
+      alert(response.message)
+      this.back()
+    } else {
+      alert(response.message)
+    }
+
+  }
+
+
+  //para eliminar
+
+  Delete(response: any) {
+    this.requestDelete(response).subscribe(
+      (response: any) => this.responseDelete(response)
+    )
+  }
+
+  requestDelete(response: any) {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.delete<any>(this.url + "/deleteMaritalStatus/" + response.idMaritalStatus + "/" + this.dataUser.user, httpOptions).pipe(
+      catchError(e => "1")
+    )
+  }
+
+  responseDelete(response: any) {
+
+    if (response.code == 999) {
+
+      this.revoke()
+    } else if (response.code == 0) {
+      alert(response.message)
+      this.back()
+    } else {
+      alert(response.message)
+    }
+  }
+
+  //formulario para agregar 
+
+  addForm() {
+    let formularioValido: any = document.getElementById("addForm");
+    if (formularioValido.reportValidity()) {
+      this.maritalStatusDataCreate.userCreation = this.dataUser.user
+      this.RequestCompanySave().subscribe(
+        (response: any) => this.ResponseCompanySave(response)
       )
     }
-    ResponseMaritalStatus(response: any) {
-      console.log("obtiene status")
-      this.maritalStatusData = response
-    console.log(this.maritalStatusData)
-  
+  }
+  RequestCompanySave() {
+
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
     }
+    return this.http.post<any>(this.url + "/createMaritalStatus", this.maritalStatusDataCreate, httpOptions).pipe(
+      catchError(e => "1")
+    )
+  }
+  ResponseCompanySave(response: any) {
+    if (response.code == 999) {
+
+      this.revoke()
+    } else if (response.code == 0) {
+      alert(response.message)
+      this.back()
+    } else {
+      alert(response.message)
+    }
+
+  }
+
+
+  //banderas
+
+  Add() {
+    this.modify = false
+    this.add = true
+    this.tab = false
+    this.header = false
+  }
+
+
+  Modify(response: any) {
+    this.maritalStatusDataModify = response
+    this.add = false
+    this.tab = false
+    this.modify = true
+    this.header = false
+  }
+
+
+  back() {
+    this.tab = true
+    this.add = false
+    this.modify = false
+    this.header = true
+    this.maritalStatusDataCreate = {}
+    this.maritalStatusDataModify = {}
+    this.ngOnInit()
+  }
+
+  backWelcome() {
+    this.router.navigateByUrl("/home")
+  }
+
+
+  //cierre de sesion
+  revoke() {
+    this.RequestRevoke().subscribe(
+      (response: any) => this.ResponseRevoke(response)
+    )
+  }
+
+
+  RequestRevoke() {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.get<any>(this.url + "/revoke/" + this.dataUser.session, httpOptions).pipe(
+      catchError(e => "1")
+    )
+  }
+
+  ResponseRevoke(response: any) {
+    if (response.code == 0) {
+      alert(response.message)
+      this.router.navigateByUrl("/")
+      localStorage.clear()
+    } else {
+      alert(response.message)
+      this.router.navigateByUrl("/")
+      localStorage.clear()
+    }
+  }
+
 
 
 
